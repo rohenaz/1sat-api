b15a03ba67bbd65ea7d1f8d67da99a7058850f67
Luke Rohenaz <rohenaz@gmail.com>
2024-01-18 07:00:33 -0500

    wip

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const ORDFS = `https://ordfs.network`;
+export const API_HOST = `https://ordinals.gorillapool.io`;
+export enum AssetType {
+  Ordinals = "ordinals",
+  BSV20 = "bsv20",
+  BSV20V2 = "bsv20v2",
+  LRC20 = "lrc20",
+}
+export enum Bsv20Status {
+  Invalid = -1,
+  Pending = 0,
+  Valid = 1,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Elysia } from 'elysia';
 import Redis from "ioredis";
+import { API_HOST, AssetType } from './constants';
+import { BSV20TXO } from './types/ordinals';
 
 // redis via url new Redis("redis://:authpassword@127.0.0.1:6380/4");
 const redis = new Redis(`${process.env.REDIS_PRIVATE_URL}`);
 
 
 const app = new Elysia()
-  .get('/market', async () => {
+  .get('/market/:assetType', async ({ params }) => {
     // check cache for results
 
     let market = await redis.get("market")
@@ -14,7 +16,7 @@ const app = new Elysia()
     // if not cached or expired, hit several market endpoints and return the aggregated data
     if (!market) {
       // store results in cache with 
-      const marketData = await fetchMarketData();
+      const marketData = await fetchMarketData(params.assetType as AssetType);
       redis.set("market", JSON.stringify(marketData), "EX", expirateionTime);
     }
   }).listen(process.env.PORT ?? 3000);
@@ -23,7 +25,39 @@ console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
 
-const fetchMarketData = async () => {
+const fetchMarketData = async (assetType: AssetType) => {
+  switch (assetType) {
+    case AssetType.BSV20:
+      const urlV1Tokens = `${API_HOST}/bsv20?limit=100&offset=0&sort=height&dir=desc&included=true`;
+      const respV1 = await fetch(urlV1Tokens);
+      const listingsV1 = await respV1.json() as BSV20TXO[];
+
+      // aggregate data
+      const marketDataV1 = listingsV1.map(listing => {
+        return {
+          tick: listing.tick,
+          price: listing.price,
+          cap: listing.amt,
+          holders: 0,
+        }
+      })
+      return marketDataV1;
+    case AssetType.BSV20V2:
+      const urlV2Tokens = `${API_HOST}/api/bsv20/v2?sort=fund_total&dir=desc&limit=20&offset=0&included=true`;
+      const resp = await fetch(urlV2Tokens);
+      const listings = await resp.json() as BSV20TXO[];
+
+      // aggregate data
+      const marketData = listings.map(listing => {
+        return {
+          tick: listing.tick,
+          price: listing.price,
+          cap: listing.amt,
+          holders: 0,
+        }
+      })
+
+  }
   return [{
     tick: "PEPE",
     price: 0.3152226666666666,
@@ -32,5 +66,5 @@ const fetchMarketData = async () => {
   }]
 }
 
-// 1 hour
-const expirateionTime = 60 * 60 * 1000;
+// 3 minutes
+const expirateionTime = 60 * 3;
