6932f908f9e7c771b2a34f53b8fdf2e8b54020b5
Luke Rohenaz <rohenaz@gmail.com>
2024-01-18 06:28:09 -0500

    add elysia

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log(Bun.env)
+import { Elysia } from 'elysia';
+
+const app = new Elysia()
+  .get('/', () => 'Eat your vegetables 🥦')
+  .listen(process.env.PORT ?? 3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
