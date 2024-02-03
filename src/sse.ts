e40b1ce7352b495215bc089201cd88190fed9b7d
Luke Rohenaz <rohenaz@gmail.com>
2024-02-03 12:04:57 -0500

    listen for listings

--- src/sse.ts
+++ src/sse.ts
@@ -6,6 +6,24 @@ import { BalanceUpdate } from "./types/bsv20";
 const sse = new EventSource(`${API_HOST}/api/subscribe?channel=v1funding&channel=v2funding`);
 
 const sseInit = async () => {
+  sse.addEventListener("listings", async (event) => {
+    console.log("Listings", event.data);
+    // const data = JSON.parse(event.data);
+    // const { listing } = data;
+    // const tick = listing.tick || listing.id;
+    // const assetType = listing.tick ? AssetType.BSV20 : AssetType.BSV20V2;
+    // const t = await redis.get(`token-${assetType}-${tick}`);
+    // const ticker = t ? JSON.parse(t) : null;
+    // if (ticker) {
+    //   ticker.listings = ticker.listings.unshift(listing);
+    //   await redis.set(`token-${AssetType.BSV20}-${tick}`, JSON.stringify(ticker), "EX", defaults.expirationTime);
+    // }
+  })
+
+  sse.addEventListener("sales", async (event) => {
+    console.log("Sales", event.data);
+  })
+
   sse.addEventListener("v1funds", async (event) => {
     const assetType = AssetType.BSV20;
     console.log("V1 Funds", event.data);
